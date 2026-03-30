a114d4cc9b26ded6117be4090ea44c5f1747f74d
ManiDeep1822 <indlachinnu123@gmail.com>
2026-03-30 10:31:26 +0530

    fix(backend): fix bug preventing shop image deletion in vendor profile

--- server/controllers/vendor.controller.js
+++ server/controllers/vendor.controller.js
@@ -207,11 +207,11 @@ const updateVendorProfile = asyncHandler(async (req, res) => {
   const vendor = await Vendor.findById(vendorId);
 
   if (vendor) {
-    if (req.body.shopImage) vendor.shopImage = req.body.shopImage;
+    if (req.body.shopImage !== undefined) vendor.shopImage = req.body.shopImage;
     if (req.body.shopName) vendor.shopName = req.body.shopName;
     if (req.body.location) vendor.location = req.body.location;
-    if (req.body.cuisineType) vendor.cuisineType = req.body.cuisineType;
-    if (req.body.operatingHours) vendor.operatingHours = req.body.operatingHours;
+    if (req.body.cuisineType !== undefined) vendor.cuisineType = req.body.cuisineType;
+    if (req.body.operatingHours !== undefined) vendor.operatingHours = req.body.operatingHours;
 
     const updatedVendor = await vendor.save();
     res.json(updatedVendor);
